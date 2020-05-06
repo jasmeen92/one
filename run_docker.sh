d36c845da10e0fa1bd50eac69ae1ff3403bceb68
jasmeen <kaurj6922@gmail.com>
2020-05-06 23:45:30 +0530

    app.py

--- run_docker.sh
+++ run_docker.sh
@@ -5,8 +5,14 @@
 # Step 1:
 # Build image and add a descriptive tag
 
+docker build - < Dockerfile -t demolocal 
+
 # Step 2: 
 # List docker images
 
+docker image ls
+
 # Step 3: 
 # Run flask app
+
+docker run -it demolocal bash
